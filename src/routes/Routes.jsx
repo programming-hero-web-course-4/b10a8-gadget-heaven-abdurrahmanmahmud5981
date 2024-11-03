c2e7925e7af1feaf7be836cc36f11babff1ae253
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-11-03 20:59:54 +0600

    all setup done alhamdulillah

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Helloo</h1>
+    }
+])
+
+export default routes;
