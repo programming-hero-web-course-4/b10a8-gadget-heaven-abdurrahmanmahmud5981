df365d33e4ae548e31a77512c88ca801507731e0
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-11-05 09:46:15 +0600

    dashboard header design completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,27 @@
-import SubHeading from "../components/SubHeading/SubHeading"
-
+import { useState } from "react";
+import SubHeading from "../components/SubHeading/SubHeading";
 
 const Dashboard = () => {
+  const [isActive,setIsActive] = useState(false);
   return (
     <div>
-      <div className="">
-        <SubHeading title="Dashboard" subTitle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"/>
+      <div className="bg-primary text-white py-10">
+        <SubHeading
+          title="Dashboard"
+          subTitle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
+        />
+        <div className=" flex justify-center items-center gap-6 mt-4">
+          <button onClick={()=>setIsActive(false)}  className={`btn rounded-full px-16  ${isActive ? "border bg-transparent text-white hover:text-primary hover:bg-white":"text-primary hover:bg-white "}`}>
+            Cart
+          </button>
+          <button onClick={()=>setIsActive(true)}   className={`btn rounded-full px-12  ${isActive ? 'text-primary hover:bg-white':"border bg-transparent text-white hover:text-primary hover:bg-white"}`}>
+            Wishlist
+          </button>
+        </div>
       </div>
       <div className=""></div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,8 +1,17 @@
+import SubHeading from "../components/SubHeading/SubHeading"
 
 
 const Statistics = () => {
   return (
-    <div>Statistics</div>
+    <div>
+      <div className="bg-primary text-white py-10">
+        <SubHeading
+          title="Statistics"
+          subTitle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
+        />
+        
+      </div>
+    </div>
   )
 }
 
